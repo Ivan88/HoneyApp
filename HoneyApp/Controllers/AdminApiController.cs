6a4499da48895a74dbb92a32ebf5eba82396bfc8
Ivan Omelchenko <omelivan@mail.ru>
2017-06-20 11:48:12 +0300

    added api controller

---
+++ HoneyApp/Controllers/AdminApiController.cs
@@ -0,0 +1,19 @@
+﻿using HoneyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HoneyApp.Controllers
+{
+    public class AdminApiController : ApiController
+    {
+		public IHttpActionResult GetSettings()
+		{
+			var settings = Enumerable.Empty<Setting>();
+			return Ok(settings);
+		}
+    }
+}
---
+++ HoneyApp/Models/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HoneyApp.Models
+{
+	public class Setting
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Availability { get; set; }
+		public string Description { get; set; }
+	}
+}
