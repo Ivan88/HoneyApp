bef2e108353296235a2fb8a6ac1b1a2a9ed60aba
Ivan Omelchenko <omelivan@mail.ru>
2017-06-21 14:39:42 +0300

    edited js and css

--- HoneyApp/App_Start/BundleConfig.cs
+++ HoneyApp/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace HoneyApp
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+						"~/Scripts/jquery-3.2.1.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
 						"~/Scripts/jquery-ui-{version}.js"));
@@ -18,12 +18,19 @@ namespace HoneyApp
 						"~/Scripts/jquery.unobtrusive*",
 						"~/Scripts/jquery.validate*"));
 
+			bundles.Add(new ScriptBundle("~/bundles/customscripts").Include(
+						"~/Scripts/admin.js",
+						"~/Scripts/styles.js"));
+
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+						"~/Content/site.css",
+						"~/Content/styles.css",
+						"~/Content/styles.scss"));
 
 			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
 						"~/Content/themes/base/jquery.ui.core.css",
