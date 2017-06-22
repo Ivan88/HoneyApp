eab2d1933e94d1ef10484349188d27509136c2f3
Ivan Omelchenko <omelivan@mail.ru>
2017-06-22 16:44:11 +0300

    little fix

--- HoneyApp/Controllers/AdminApiController.cs
+++ HoneyApp/Controllers/AdminApiController.cs
@@ -1,10 +1,7 @@
-﻿using Models;
-using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using XmlDataProvider;
 
 namespace HoneyApp.Controllers
 {
---
+++ XmlDataProvider/Setting.cs
@@ -0,0 +1,11 @@
+﻿
+namespace XmlDataProvider
+{
+	public class Setting
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Availability { get; set; }
+	}
+}
